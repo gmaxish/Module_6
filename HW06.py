33124215e3336e0043a933a8b2f723127b2dbdd3
gmaxish <gm2013@ukr.net>
2022-03-30 15:59:17 +0300

    35 day of war

--- HW06.py
+++ HW06.py
@@ -3,16 +3,36 @@
 данных должна осуществляться в отдельных функциях, принимающих следующие параметры: имя пользователя, пароль,
 количество попыток входа в систему (по умолчанию = 3), сообщение, выводимое пользователю в случае, если все попытки
 входа в систему исчерпаны (по умолчанию: "Система заблокирована. Повторите попытку черезе 5 минут.")
-
 """
-login = input("Enter your login: ")
-pswd = input("Enter your password: ")
 
 
-def check_login(t):
+def check_login(login, trys=2, reminder="Система заблокирована. Повторите попытку черезе 5 минут."):
     while True:
-        if len(t) >= 5:
+        l = input(login)
+        if len(l) >= 5:
             print("Login was set")
             return True
         else:
-            print("Your login is too short. Try another")
+            print("Login is too short. Try another")
+            trys -= 1
+        if trys < 0:
+            print(reminder)
+            break
+
+
+def check_password(pas, trys=2, reminder="Система заблокирована. Повторите попытку черезе 5 минут."):
+    while True:
+        p = input(pas)
+        if len(p) >= 5:
+            print("Password was set")
+            return True
+        else:
+            print("Password is too short. Try another")
+            trys -= 1
+        if trys < 0:
+            print(reminder)
+            break
+
+
+if check_login("Enter your login: ") is True and check_password("Enter your password: ") is True:
+    print("OK!")
